90ea0b2c906191fc6c89390d8a0ddd035b40e9d1
Jalagaban <jalagaban@gmail.com>
2014-10-04 21:33:20 -0400

    Wrote LoadAll, Update, Create, and Delete functions

--- AddressBook/AddressEntry.cs
+++ AddressBook/AddressEntry.cs
@@ -40,6 +40,7 @@ namespace AddressBook
                 return instance;
             }
         }
+        //test connection to database
         public bool testConnection()
         {
             SqlCeConnection db = new SqlCeConnection("Data Source = AddressEntry.sdf");
@@ -59,12 +60,78 @@ namespace AddressBook
         {
             //return a List of AddressEntry objects populated with data from AddressEntry database
             List<AddressEntry> addrBook = new List<AddressEntry>();
+            SqlCeConnection db = new SqlCeConnection("Data Source = AddressEntry.sdf");
+            db.Open();
+            SqlCeCommand cmd = new SqlCeCommand("Select * from AddressEntry", db);
+            using (SqlCeDataReader rd = cmd.ExecuteReader())
+            {
+                while (rd.Read())
+                {
+                    string name = rd.GetString(0);
+                    string emAddr = rd.GetString(1);
+                    AddressEntry entry = new AddressEntry(name, emAddr);
+                    addrBook.Add(entry);
+                }
+            }
+            db.Close();
             return addrBook;
         }
 
         //Update a record
+        public static bool UpdateRecord(AddressEntry ent)
+        {
+            try
+            {
+                SqlCeConnection db = new SqlCeConnection("Data Source = AddressEntry.sdf");
+                db.Open();
+                SqlCeCommand cmd = db.CreateCommand();
+                cmd.CommandText = @"Update AddressEntry 
+                                    Set name = @ent.getName(), EmailAddr = @ent.getAddr() 
+                                    where name = @ent.getName() and EmailAddr = @ent.getAddr())"; //Update SQL command
+                cmd.ExecuteNonQuery();
+                db.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
         //Create a new record
+        public static bool CreateRecord(AddressEntry ent)
+        {
+            try
+            {
+                SqlCeConnection db = new SqlCeConnection("Data Source = AddressEntry.sdf");
+                db.Open();
+                SqlCeCommand cmd = new SqlCeCommand("Insert into AddressEntry Values('@ent.getName()','@ent.getAddr()')", db); //Insert SQL command
+                cmd.ExecuteNonQuery();
+                db.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
         //Delete a record
+        public static bool DeleteRecord(AddressEntry ent)
+        {
+            try
+            {
+                SqlCeConnection db = new SqlCeConnection("Data Source = AddressEntry.sdf");
+                db.Open();
+                SqlCeCommand cmd = db.CreateCommand();
+                cmd.CommandText = @"Delete from AddressEntry where Name = @ent.getName() and EmailAddr = @ent.getAddr()"; //Delete SQL command
+                cmd.ExecuteNonQuery();
+                db.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
         //Update, Create, Delete methods uses AddressEntry parameter, extract information from instance, and issue calls
     }
 }
