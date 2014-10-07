7b7822d5d64e9efdebd5b9094b2757fe857a73f0
Nathaniel Palmer <nathanielpalmer@me.com>
2014-10-07 17:07:42 -0400

    Adjust button-form spacing on EditWindow form

--- AddressBook/EditWindow.Designer.cs
+++ AddressBook/EditWindow.Designer.cs
@@ -76,7 +76,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(279, 68);
+            this.ClientSize = new System.Drawing.Size(279, 73);
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cancelButton);
