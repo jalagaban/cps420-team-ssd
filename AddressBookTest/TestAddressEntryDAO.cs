1a60999a535eae18b0687992273bc5db5e98ced1
Nathaniel Palmer <nathanielpalmer@me.com>
2014-10-06 22:26:58 -0400

    Implement TestAddressEntryDAO.TestAll() unit test

--- AddressBookTest/TestAddressEntryDAO.cs
+++ AddressBookTest/TestAddressEntryDAO.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AddressBook;
 
@@ -8,9 +9,21 @@ namespace AddressBookTest
     public class TestAddressEntryDAO
     {
         [TestMethod]
-        public void TestBasic()
+        public void TestAll()
         {
-             
+            AddressEntry entry = new AddressEntry("cow", "barn");
+            Assert.IsTrue(AddressEntryDAO.CreateRecord(entry), "CreateRecord() failed");
+
+            Assert.AreEqual<AddressEntry>(entry, AddressEntryDAO.LoadAll()[0], "newly created record could not be found");
+
+            AddressEntry modifiedEntry = new AddressEntry("cow", "pen");
+            Assert.IsTrue(AddressEntryDAO.UpdateRecord(modifiedEntry), "UpdateRecord() failed");
+
+            Assert.AreEqual<AddressEntry>(modifiedEntry, AddressEntryDAO.LoadAll()[0], "updated record could not be found");
+
+            Assert.IsTrue(AddressEntryDAO.DeleteRecord(modifiedEntry), "DeleteRecord() failed");
+
+            Assert.AreEqual(new List<AddressEntry> { }, AddressEntryDAO.LoadAll(), "record could not be deleted");
         }
     }
 }
