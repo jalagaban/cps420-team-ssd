9d94e6fb575fcda5bc7e765126a850fc6a16a68e
Nathaniel Palmer <nathanielpalmer@me.com>
2014-10-06 22:00:37 -0400

    Add AddressBookTest project to solution

---
+++ AddressBookTest/TestAddressEntryDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AddressBook;
+
+namespace AddressBookTest
+{
+    [TestClass]
+    public class TestAddressEntryDAO
+    {
+        [TestMethod]
+        public void TestBasic()
+        {
+             
+        }
+    }
+}
