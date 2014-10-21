5d72cafd72a81af511fda1f0fbc6e12ded7be18f
luke.stayer@gmail.com <luke.stayer@gmail.com>
2014-10-21 15:40:42 -0400

    Renamed to Prototype

--- Prototype/Prototype/Properties/AssemblyInfo.cs
+++ Prototype/Prototype/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CpS 420 Inception Project")]
+[assembly: AssemblyTitle("Prototype")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CpS 420 Inception Project")]
+[assembly: AssemblyProduct("Prototype")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
