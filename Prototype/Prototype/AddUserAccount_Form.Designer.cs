e5d15bedde7052ce67ba08cf46ceae166af61dac
Nathaniel Palmer <nathanielpalmer@me.com>
2014-10-22 00:11:19 -0400

    Add First Name and Last Name fields to AddUserAccount_Form

--- Prototype/Prototype/AddUserAccount_Form.Designer.cs
+++ Prototype/Prototype/AddUserAccount_Form.Designer.cs
@@ -36,6 +36,10 @@
             this.Submit_Button = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.Username_Combobox = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // UserAccounts_Label
@@ -45,14 +49,14 @@
             this.UserAccounts_Label.ForeColor = System.Drawing.Color.SteelBlue;
             this.UserAccounts_Label.Location = new System.Drawing.Point(12, 9);
             this.UserAccounts_Label.Name = "UserAccounts_Label";
-            this.UserAccounts_Label.Size = new System.Drawing.Size(240, 45);
+            this.UserAccounts_Label.Size = new System.Drawing.Size(101, 45);
             this.UserAccounts_Label.TabIndex = 1;
-            this.UserAccounts_Label.Text = "User Accounts:";
+            this.UserAccounts_Label.Text = "Users";
             // 
             // Password_Textbox
             // 
             this.Password_Textbox.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Password_Textbox.Location = new System.Drawing.Point(98, 110);
+            this.Password_Textbox.Location = new System.Drawing.Point(106, 94);
             this.Password_Textbox.Name = "Password_Textbox";
             this.Password_Textbox.Size = new System.Drawing.Size(104, 22);
             this.Password_Textbox.TabIndex = 2;
@@ -63,7 +67,7 @@
             this.CashierNumber_Label.AutoSize = true;
             this.CashierNumber_Label.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CashierNumber_Label.ForeColor = System.Drawing.Color.SteelBlue;
-            this.CashierNumber_Label.Location = new System.Drawing.Point(19, 112);
+            this.CashierNumber_Label.Location = new System.Drawing.Point(19, 96);
             this.CashierNumber_Label.Name = "CashierNumber_Label";
             this.CashierNumber_Label.Size = new System.Drawing.Size(73, 20);
             this.CashierNumber_Label.TabIndex = 34;
@@ -74,7 +78,7 @@
             this.Username_Label.AutoSize = true;
             this.Username_Label.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Username_Label.ForeColor = System.Drawing.Color.SteelBlue;
-            this.Username_Label.Location = new System.Drawing.Point(16, 75);
+            this.Username_Label.Location = new System.Drawing.Point(19, 67);
             this.Username_Label.Name = "Username_Label";
             this.Username_Label.Size = new System.Drawing.Size(76, 20);
             this.Username_Label.TabIndex = 32;
@@ -85,7 +89,7 @@
             this.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.Cancel_Button.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Cancel_Button.ForeColor = System.Drawing.Color.SteelBlue;
-            this.Cancel_Button.Location = new System.Drawing.Point(158, 194);
+            this.Cancel_Button.Location = new System.Drawing.Point(158, 227);
             this.Cancel_Button.Name = "Cancel_Button";
             this.Cancel_Button.Size = new System.Drawing.Size(120, 40);
             this.Cancel_Button.TabIndex = 5;
@@ -97,7 +101,7 @@
             this.Submit_Button.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.Submit_Button.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Submit_Button.ForeColor = System.Drawing.Color.SteelBlue;
-            this.Submit_Button.Location = new System.Drawing.Point(12, 194);
+            this.Submit_Button.Location = new System.Drawing.Point(12, 227);
             this.Submit_Button.Name = "Submit_Button";
             this.Submit_Button.Size = new System.Drawing.Size(121, 40);
             this.Submit_Button.TabIndex = 4;
@@ -110,7 +114,7 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.checkBox1.ForeColor = System.Drawing.Color.SteelBlue;
-            this.checkBox1.Location = new System.Drawing.Point(20, 151);
+            this.checkBox1.Location = new System.Drawing.Point(20, 184);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(245, 21);
             this.checkBox1.TabIndex = 3;
@@ -123,16 +127,60 @@
             this.Username_Combobox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.Username_Combobox.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Username_Combobox.FormattingEnabled = true;
-            this.Username_Combobox.Location = new System.Drawing.Point(99, 73);
+            this.Username_Combobox.Location = new System.Drawing.Point(107, 67);
             this.Username_Combobox.Name = "Username_Combobox";
             this.Username_Combobox.Size = new System.Drawing.Size(103, 21);
             this.Username_Combobox.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(106, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 22);
+            this.textBox1.TabIndex = 35;
+            this.textBox1.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(18, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "First Name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(106, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(104, 22);
+            this.textBox2.TabIndex = 37;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label2.Location = new System.Drawing.Point(19, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Last Name:";
             // 
             // AddUserAccount_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(291, 246);
+            this.ClientSize = new System.Drawing.Size(291, 279);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Username_Combobox);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.Cancel_Button);
@@ -141,10 +189,10 @@
             this.Controls.Add(this.CashierNumber_Label);
             this.Controls.Add(this.Username_Label);
             this.Controls.Add(this.UserAccounts_Label);
-            this.MaximumSize = new System.Drawing.Size(307, 285);
-            this.MinimumSize = new System.Drawing.Size(307, 285);
+            this.MaximumSize = new System.Drawing.Size(307, 317);
+            this.MinimumSize = new System.Drawing.Size(307, 317);
             this.Name = "AddUserAccount_Form";
-            this.Text = "Create/Modify User Account";
+            this.Text = "Manage Users";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,5 +208,9 @@
         private System.Windows.Forms.Button Submit_Button;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.ComboBox Username_Combobox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
