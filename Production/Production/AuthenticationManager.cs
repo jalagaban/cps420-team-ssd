2eb381f66d13b69a91dc4f17d1384d9eea73bb14
Nathaniel Palmer <nathanielpalmer@me.com>
2014-11-07 02:52:46 -0500

    write pseudocode for model class methods

--- Production/Production/AuthenticationManager.cs
+++ Production/Production/AuthenticationManager.cs
@@ -12,19 +12,40 @@ namespace Production
     public class AuthenticationManager
     {
         // Attempts to log in using the provided credentials. Returns true if login was successful and false if login failed.
-        public bool AttemptLogin(string username, string password);
+        public bool AttemptLogin(string username, string password)
+        {
+            // Get user for given username
+            // Check user.Password against given password
+                // If equal, set CurrentUser to user and return true
+                // else return false
+        }
 
-        public void LogOut();
+        public void LogOut()
+        {
+            // Set CurrentUser to null
+        }
 
         // The currently logged-in user
         public User CurrentUser { get; set; }
 
-        public User GetUser(string username);
+        public User GetUser(string username)
+        {
+            // Get user from database where user.username == username
+        }
 
-        public void AddUser(User user);
+        public void AddUser(User user)
+        {
+            // Insert user into database
+        }
 
-        public void UpdateUser(User user);
+        public void UpdateUser(User user)
+        {
+            // Replace User table row in database with user where UserID column == user.Username
+        }
 
-        public void DeleteUser(string username);
+        public void DeleteUser(string username)
+        {
+            // Delete user from database where user.username == username
+        }
     }
 }
--- Production/Production/DatabaseAgent.cs
+++ Production/Production/DatabaseAgent.cs
@@ -15,39 +15,76 @@ namespace Production
          These attributes as ordered in the tuple are the RoutingNum, AccountNum, and CheckNum. */
         public List<Tuple<string, string, string>> CheckKeys;
 
-        public Check GetCheck(Tuple<string, string, string> key);
+        public Check GetCheck(Tuple<string, string, string> key)
+        {
+            // Get check from database where check.RoutingNum == key[0], check.AccountNum == key[1], and check.CheckNum == key[2] 
+        }
 
-        public void AddCheck(Check check);
+        public void AddCheck(Check check)
+        {
+            // Insert check into database
+        }
 
-        public void UpdateCheck(Check check);
+        public void UpdateCheck(Check check)
+        {
+            // Replace Check table row in database with check where RoutNum column == check.RoutingNum, AcctNum column == check.AccountNum, and  ChkNum column == check.CheckNum
+        }
 
-        public void DeleteCheck(Tuple<string, string, string> key);
+        public void DeleteCheck(Tuple<string, string, string> key)
+        {
+            // Delete check from database where check.RoutingNum == key[0], check.AccountNum == key[1], and check.CheckNum == key[2] 
+        }
 
         
         /* A list containing the primary keys of all accounts in the database. 
         Each key is a tuple containing the two identifiying attributes of an account.
         These attributes as ordered in the tuple are the RoutingNum and AccountNum. */
         public List<Tuple<string, string>> AccountKeys;
-        
-        public Account GetAccount(Tuple<string, string> key);
 
-        public void AddAccount(Account account);
+        public Account GetAccount(Tuple<string, string> key)
+        {
+            // Get account from database where account.RoutingNum == key[0] and account.AccountNum == key[1]
+        }
 
-        public void UpdateAccount(Account account);
+        public void AddAccount(Account account)
+        {
+            // Insert account into database
+        }
 
-        public void DeleteAccount(Tuple<string, string> key);
+        public void UpdateAccount(Account account)
+        {
+            // Replace Account table row in database with account where RoutNum column == account.RoutingNum and AcctNum column == account.AccountNum
+        }
+
+        public void DeleteAccount(Tuple<string, string> key)
+        {
+            // Delete account from database where account.RoutingNum == key[0] and account.AccountNum == key[1]
+        }
 
 
         /* A list containing the primary keys of all stores in the database. 
          The primary key for a store is equivalent to its StoreNum. */
         public string StoreKeys;
 
-        public Store GetStore(string storeNum);
+        public Store GetStore(string storeNum)
+        {
+            // Get store from database where store.ID == storeNum
+        }
 
-        public void AddStore(Store store);
+        public void AddStore(Store store)
+        {
+            // Insert store into database
+        }
 
-        public void UpdateStore(Store store);
+        public void UpdateStore(Store store)
+        {
+            // Replace STore table row in database with store where StoreID column == store.ID
+        }
 
-        public void DeleteStore(string storeNum);
+        public void DeleteStore(string storeNum)
+        {
+            // Delete store from database where store.ID == storeNum
+
+        }
     }
 }
