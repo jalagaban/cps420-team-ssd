27120d83f8f8b0b08d642af8695ec213926df708
Nathaniel Palmer <nathanielpalmer@me.com>
2014-12-04 16:16:51 -0500

    change executable name to "cps"

--- Production/Production/Properties/Settings.Designer.cs
+++ Production/Production/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Production.Properties {
+namespace cps.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
