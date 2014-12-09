3cb4f55d733888a8c7372cd07f5b8b84c0096890
Jalagaban <jalagaban@gmail.com>
2014-12-09 09:41:48 -0500

    Printing is still WIP

---
+++ Production/Production/Document.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Production
+{
+    //Object to hold letter-related information pulled from database
+    public class Document
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+        public int ChkAmt { get; set; }
+
+        public DateTime ChkDate { get; set; }
+
+        public string StoreName { get; set; }
+
+        public string CompName { get; set; }
+
+        public string CompStreet { get; set; }
+
+        public string CompCity { get; set; }
+
+        public string CompState { get; set; }
+
+        public string CompZip { get; set; }
+
+        public int CompFee { get; set; }
+
+
+    }
+}
---
+++ Production/Production/PrintLetters.cs
@@ -0,0 +1,101 @@
+﻿//Handles Print click event
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+using System.Drawing;
+using Production;
+
+namespace CpS_420_Inception_Project
+{
+    class PrintLetters
+    {
+        private static PrintLetters printAgent;
+
+        public static PrintLetters PrintAgent
+        {
+            get
+            {
+                if (printAgent == null)
+                {
+                    printAgent = new PrintLetters();
+                }
+                return printAgent;
+            }
+        }
+        int counter = 0;
+
+        //Used: http://www.codeproject.com/Tips/453871/Simple-Receipt-Like-Printing-Using-the-Csharp-Prin as reference
+        public void PrintDocuments()
+        {
+            PrintDialog pd = new PrintDialog();
+            PrintDocument doc = new PrintDocument();
+            Font font = new Font("Calibri", 12);
+
+            PaperSize psize = new PaperSize("Custom", 100, 200);
+
+            pd.Document = doc;
+            pd.Document.DefaultPageSettings.PaperSize = psize;
+
+            doc.DefaultPageSettings.PaperSize.Height = 820;
+            doc.DefaultPageSettings.PaperSize.Width = 520;
+
+            doc.PrintPage += new PrintPageEventHandler(GeneratePage);
+            DialogResult result = pd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                PrintPreviewDialog pp = new PrintPreviewDialog();
+                pp.Document = doc;
+                result = pp.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    doc.Print();
+                }
+            }
+        }
+
+        public void GeneratePage(object sender, PrintPageEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Font font = new Font("Calibri", 12);
+            float fontHeight = font.GetHeight();
+
+            int startX = 50;
+            int startY = 55;
+            int Offset = 40;
+
+            graphics.DrawString("DEMAND LETTER", new Font("Calibri", 18), new SolidBrush(Color.Black), startX, startY + Offset);
+            PrintManager pm = PrintManager.DefaultPrintManager;
+            List<Document> documents = pm.GetLettersToPrint();
+
+            while (counter < documents.Count)
+            {
+                Offset = Offset + 20;
+                graphics.DrawString("Test", new Font("Calibri", 12), new SolidBrush(Color.Black), startX, startY + Offset);
+
+                if (counter == documents.Count - 1) { e.HasMorePages = false; }
+                else { e.HasMorePages = true; return; }
+
+                counter++;
+            }
+
+        }
+        /*public void PrintLetters()
+        {
+            PrintManager pm = PrintManager.DefaultPrintManager;
+            List<Document> documents = pm.GetLettersToPrint();
+            foreach (Document doc in documents)
+            {
+                PrintDocuments(doc);
+            }
+        }*/
+    }
+}
